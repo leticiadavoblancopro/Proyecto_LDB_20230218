af368391aaff281b54fa1b52d67196c6e042bae9
leticiadavoblancopro <leticiadavoblancopro@gmail.com>
2023-02-18 20:39:02 +0100

    pruebacambio
    Se añade una nueva clase al proyecto para verificar que funciona el proceso

---
+++ src/Proyecto_LDB_20230218/PruebaGit.java
@@ -0,0 +1,12 @@
+package Proyecto_LDB_20230218;
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Nueva clase de prueba Git");
+	}
+
+}
