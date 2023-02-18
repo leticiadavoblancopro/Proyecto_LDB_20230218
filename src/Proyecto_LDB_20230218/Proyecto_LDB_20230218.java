7d74c3791f13d856ecac7cecb67592b3831b0abc
leticiadavoblancopro <leticiadavoblancopro@gmail.com>
2023-02-18 20:25:01 +0100

    Initial commit

---
+++ src/Proyecto_LDB_20230218/Proyecto_LDB_20230218.java
@@ -0,0 +1,14 @@
+package Proyecto_LDB_20230218;
+
+public class Proyecto_LDB_20230218 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Practica GIT y GitHub");	
+		
+		
+		
+	}
+
+}
